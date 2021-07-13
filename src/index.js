a939c6eb01966e44b5eee26af5ec5e9439e2e9ef
Elena Zvonenko <elena.zvonenko15@gmail.com>
2021-07-13 18:33:54 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 function generateList(array) {
-    const list = document.createElement('ul');
+    const $list = document.createElement('ul');
     array.forEach(element => {
-        const listChild = document.createElement('li');
-        list.append(listChild);
+        const $listChild = document.createElement('li');
+        $list.append($listChild);
         if (Array.isArray(element)) {
-            listChild.append(generateList(element));
+            $listChild.append(generateList(element));
         }
         else {
-            listChild.innerText = element;
+            $listChild.innerText = element;
         }
     });
-    return list;
+    return $list;
 }
 
 const arr = ['1', '2', ['1.1', ['2.1', '2.2', '2.3'], '1.2', '1.3'], '3'];
@@ -23,17 +23,17 @@ function createTable() {
     let counter = 1;
     const startIndex = 0;
     const endIndex = 10;
-    const table = document.createElement('table');
-    document.body.append(table);
+    const $table = document.createElement('table');
     for (let i = startIndex; i < endIndex; i++) {
-        const row = document.createElement('tr');
-        table.append(row);
+        const $row = document.createElement('tr');
+        $table.append($row);
         for (let j = startIndex; j < endIndex; j++) {
-            const cell = document.createElement('td');
-            cell.innerText = counter++;
-            row.append(cell);
+            const $cell = document.createElement('td');
+            $cell.innerText = counter++;
+            $row.append($cell);
         }
     }
+    return $table;
 }
 
-createTable();
+document.body.append(createTable());
